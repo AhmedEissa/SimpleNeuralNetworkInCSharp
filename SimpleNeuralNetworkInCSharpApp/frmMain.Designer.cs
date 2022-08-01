12bb0804da3fba703c0d3e2d5c7a454983eafc7e
Ahmed Eissa <eissamail@googlemail.com>
2022-08-01 14:34:19 +0100

    Addded the Workflow code from the article section 4.7 in a Winform project.

---
+++ SimpleNeuralNetworkInCSharpApp/frmMain.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace SimpleNeuralNetworkInCSharpApp
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(12, 12);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(290, 247);
+            this.txtOutput.TabIndex = 0;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(308, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(390, 277);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.txtOutput);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "frmMain";
+            this.Text = "Simple Neural Network In C#";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Button btnStart;
+    }
+}
+
---
+++ SimpleNeuralNetworkInCSharpApp/frmMain.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Windows.Forms;
+using NeuralNetworkCSharp;
+using NeuralNetworkCSharp.ActivationFunctions;
+using NeuralNetworkCSharp.InputFunctions;
+
+namespace SimpleNeuralNetworkInCSharpApp
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            txtOutput.Text = "Starting...";
+            this.Cursor = Cursors.WaitCursor;
+            var network = new SimpleNeuralNetwork(3);
+            var layerFactory = new NeuralLayerFactory();
+            network.AddLayer(layerFactory.CreateNeuralLayer(3, new RectifiedActivationFuncion(), new WeightedSumFunction()));
+            network.AddLayer(layerFactory.CreateNeuralLayer(1, new SigmoidActivationFunction(0.7), new WeightedSumFunction()));
+            
+            network.PushExpectedValues(
+                                        new double[][] {
+                                        new double[] { 0 },
+                                        new double[] { 1 },
+                                        new double[] { 1 },
+                                        new double[] { 0 },
+                                        new double[] { 1 },
+                                        new double[] { 0 },
+                                        new double[] { 0 },
+                                        });
+
+            network.Train(
+                            new double[][] {
+                            new double[] { 150, 2, 0 },
+                            new double[] { 1002, 56, 1 },
+                            new double[] { 1060, 59, 1 },
+                            new double[] { 200, 3, 0 },
+                            new double[] { 300, 3, 1 },
+                            new double[] { 120, 1, 0 },
+                            new double[] { 80, 1, 0 },
+                            }, 10000);
+
+            network.PushInputValues(new double[] { 1054, 54, 1 });
+            var outputs = network.GetOutput();
+            foreach(var output in outputs)
+            {
+                txtOutput.Text = output.ToString();
+            }
+            this.Cursor = Cursors.Default;
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            txtOutput.Text = "Ready.";
+        }
+    }
+}
